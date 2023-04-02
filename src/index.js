e6bc8947e16f4a67c1dc133cb456eb7635bcd764
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-04-02 11:25:57 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import { fetchImages } from './fetchImages';
 const refs = {
   formSearch: document.querySelector('.search-form'),
   buttonSearch: document.querySelector('.search-button'),
+  inputSearch: document.querySelector('.search-input'),
 };
 
 const enterDataSearchImage = event => {
   event.preventDefault();
 
-  fetchImages(name);
-  console.log('qwqw');
+  const searchImage = refs.inputSearch.value.trim();
+
+  fetchImages(searchImage);
+  console.log(searchImage);
 };
 
 refs.formSearch.addEventListener('submit', enterDataSearchImage);
